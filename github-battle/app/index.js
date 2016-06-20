7799e2470b526a68d3e911fa8d7da319c1ed9e96
Denis Nunes <denis_tecnosoft@hotmail.com>
2016-06-20 09:58:51 -0300

    Passing Props to children

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,15 +1,36 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var Container = React.createClass({
   render: function () {
+    var nomes = ['Denis', 'Rex', 'Piririca', 'Guto'];
+
     return (
-      <h1> Hello World </h1>
-    )
+      <List nomes={nomes} />
+    );
+  }
+});
+
+
+
+var List = React.createClass({
+  render: function () {
+    var listNames = this.props.nomes.map( function (el) {
+      return <li> {el} </li>;
+    });
+
+    return (
+      <div>
+        <h1> Lista </h1>
+        <ul>
+          {listNames}
+        </ul>
+      </div>
+    );
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Container />,
   document.getElementById('app')
 );
